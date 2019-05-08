06881e49da4310921728faa8e8480fd31e48c4b7
JakobDevelops <jakobwiemer@web.de>
2019-05-08 09:57:45 +0800

    Initial Commit

---
+++ checkwise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  checkwise
+//
+//  Created by Jakob Wiemer on 08.05.19.
+//  Copyright © 2019 Jakob Wiemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
